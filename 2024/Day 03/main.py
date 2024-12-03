a46c114242c5ba8d83344f0418af1b879b725ab6
Noah Jorgensen <noduh.jorgensen@gmail.com>
2024-12-02 22:56:49 -0700

    feat: wrote function to check validity of input and return numbers if it's valid

--- 2024/Day 03/main.py
+++ 2024/Day 03/main.py
@@ -14,7 +14,29 @@ def get_command_inputs(command: str, input_string: str) -> list[str]:
 
 def get_multiply_data(command_input: str) -> tuple[bool, tuple[int, int]]:
     """checks if the input is valid for the multiply command"""
-    return True, None
+    x = None
+    y = None
+    can_multiply = True
+    input_string = command_input # will no longer remove parentheses. this will happen before the function is called
+    input_data = []
+
+    # check for invalid whitespace
+    if input_string == input_string.strip():
+        input_string = input_string.replace(" ", ".") # helps casting fail invalid inputs with extra whitespace
+        input_data = input_string.split(",")
+    else:
+        can_multiply = False
+
+    if not can_multiply and len(input_data) != 2:
+        can_multiply = False
+    else:
+        try:
+            x = int(input_data[0])
+            y = int(input_data[1])
+        except ValueError:
+            can_multiply = False
+
+    return can_multiply, (x, y)
 
 
 def multiply(x: int, y: int) -> int:
@@ -46,3 +68,17 @@ def main():
 
 
 main()
+
+
+# # TESTING
+# can_multiply, (x, y) = get_multiply_data("10, 15")
+# print(f"{can_multiply} | {x}*{y}")
+
+# can_multiply, (x, y) = get_multiply_data("10,15")
+# print(f"{can_multiply} | {x}*{y}")
+
+# can_multiply, (x, y) = get_multiply_data(" 10,15")
+# print(f"{can_multiply} | {x}*{y}")
+
+# can_multiply, (x, y) = get_multiply_data("a10,15")
+# print(f"{can_multiply} | {x}*{y}")
