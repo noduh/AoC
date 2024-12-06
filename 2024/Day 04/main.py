a213edf9276bc5d0b69bb84db2b3ccf03036b7ae
Noah <njorgensen@byui.edu>
2024-12-05 19:47:10 -0700

    feat: wrote the function that searches starting at a specific letter

--- 2024/Day 04/main.py
+++ 2024/Day 04/main.py
@@ -8,10 +8,38 @@ def get_challenge_input(file_path: str) -> list[str]:
 
 
 def search_letter(
-    line: int, string_location: int, word_to_search: str, challenge_input: list[str]
+    line_start: int, char_start: int, word_to_search: str, challenge_input: list[str]
 ) -> int:
     """Takes the location of the letter to search, and checks each direction in the input. It returns the number of times the letter leads to a full result of the searched word"""
-    return 0
+    NORTH = 0, 1
+    NORTHEAST = 1, 1
+    EAST = 1, 0
+    SOUTHEAST = 1, -1
+    SOUTH = 0, -1
+    SOUTHWEST = -1, -1
+    WEST = -1, 0
+    NORTHWEST = -1, 1
+    directions = [NORTH, NORTHEAST, EAST, SOUTHEAST, SOUTH, SOUTHWEST, WEST, NORTHWEST]
+
+    # Automatically return 0 if it doesn't work in any direction
+    if challenge_input[line_start][char_start] != word_to_search[0]:
+        return 0
+
+    for multiplier, letter in enumerate(
+        word_to_search, 1
+    ):  # multiplier lets us find the specific line and character easily
+        for direction in directions:
+            line_direction, char_direction = direction
+            line_index = line_direction * multiplier + line_start
+            char_num = char_direction * multiplier + char_start
+            if 0 > line_index > len(challenge_input) or 0 > char_num > len(
+                challenge_input[line_index]
+            ):
+                directions.remove(direction)
+            elif challenge_input[line_index][char_num] != letter:
+                directions.remove(direction)
+
+    return len(directions)
 
 
 def search_input(word_to_search: str, challenge_input: list[str]) -> int:
@@ -27,3 +55,8 @@ def main():
 
 
 main()
+
+# # tests
+# print(
+#     search_letter(0, 4, "XMAS", get_challenge_input("2024\Day 04\challenge_input.txt"))
+# )
