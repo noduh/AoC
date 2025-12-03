40e5ec316a10806d6205422d3647211b8a1e42e8
Noah Jorgensen <noduh.jorgensen@gmail.com>
2025-12-03 16:13:22 -0700

    created a function to split strings into equal parts. currently the validity check is broken

--- 2025/Day 2/main.py
+++ 2025/Day 2/main.py
@@ -1,6 +1,26 @@
 INPUT_FILE_LOCATION = "2025/Day 2/input.txt"
 
 
+def split_string(string_to_split: str, substring_length: int) -> list[str]:
+    """
+    Splits the string into even parts and returns a list
+
+    :param string_to_split: the original string to split it into
+    :type string_to_split: str
+    :param substring_length: the preferred length of each substring
+    :type substring_length: int
+    :return: a list of substrings taken from the original string
+    :rtype: list[str]
+    """
+    substrings = []
+    while len(string_to_split) >= substring_length:
+        substring = string_to_split[:substring_length]
+        substrings.append(substring)
+        string_to_split = string_to_split[substring_length:]
+    substrings.append(string_to_split)
+    return substrings
+
+
 def get_id_ranges(file_location: str) -> list[tuple[int, int]]:
     """
     Gets IDs from the given input file
@@ -22,12 +42,23 @@ def get_id_ranges(file_location: str) -> list[tuple[int, int]]:
 
 
 def check_id_validity(id: int) -> bool:
+    """
+    Checks the validity of the id
+
+    :param id: the id to check
+    :type id: int
+    :return: returns a boolean that says if it's a valid id
+    :rtype: bool
+    """
     id_string = str(id)
     id_string_length = len(id_string)
-    if id_string_length % 2 != 0:  # if the id is an odd length, it must be valid
-        return True
-    if id_string[(id_string_length // 2) :] == id_string[: (id_string_length // 2)]:
-        return False
+    for i in range(1, id_string_length // 2):
+        if id_string_length % i != 0:  # if the id is not divisible, it must be valid
+            return True
+        id_string_substrings = split_string(id_string, i)
+        for substring in id_string_substrings:
+            if substring != id_string_substrings[0]:
+                return False
     return True
 
 
@@ -48,4 +79,8 @@ def main():
     print(sum_invalid_ids)
 
 
-main()
+# main()
+print(check_id_validity(123123123)) # false
+print(check_id_validity(1231231234)) # true
+print(check_id_validity(1212121212)) # false
+print(check_id_validity(12345678)) # true
