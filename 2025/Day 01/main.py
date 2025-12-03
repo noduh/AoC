af30cc65d93c138bdc3c1ddc7653023d6eaaf5e0
Noah Jorgensen <noduh.jorgensen@gmail.com>
2025-12-03 12:50:52 -0700

    not working version, but need to be able to come back

--- 2025/Day 01/main.py
+++ 2025/Day 01/main.py
@@ -23,14 +23,36 @@ def get_steps(file_location: str) -> list[tuple[int, int]]:
 
     return steps
 
+def get_sign(number: int | float) -> int:
+    """
+    Docstring for get_sign
+    
+    :param number: any real number
+    :type number: int | float
+    :return: either 1 or -1 to indicate positive or negative, or 0 to indicate neither
+    :rtype: int
+    """
+    if number == 0:
+        return 0
+    return int(number / abs(number))
 
 def main():
     current_location = 50
     password = 0
-    steps = get_steps("2025/Day 01/input.txt")
+    steps = get_steps("2025/Day 01/test_input.txt")
     for step in steps:
-        current_location = (current_location + step[0] * step[1]) % 100
-        if current_location == 0:
+        previous_sign = get_sign(current_location)
+        distance = step[1]
+        direction = step[0]
+        password += distance // 100 # accounts for distances greater than 100
+        distance = distance % 100   # 󱟀
+        current_location = (current_location + direction * distance) # gets new location
+        if current_location != current_location % 100 and current_location % 100 != 0: # checks if simplifying indicates passing 0
+            password += 1
+            current_location = current_location % 100 # simplifies
+        elif current_location != 0 and get_sign(current_location) != previous_sign: # checks if the sign has fully changed
+            password += 1
+        elif current_location == 0:
             password += 1
     print(password)
 
