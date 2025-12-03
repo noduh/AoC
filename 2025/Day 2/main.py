7d144e361f94ced387b57be1dbff8ee95f327b7d
Noah Jorgensen <noduh.jorgensen@gmail.com>
2025-12-03 15:07:45 -0700

    solved part 1

--- 2025/Day 2/main.py
+++ 2025/Day 2/main.py
@@ -29,3 +29,22 @@ def check_id_validity(id: int) -> bool:
     if id_string[(id_string_length // 2) :] == id_string[: (id_string_length // 2)]:
         return False
     return True
+
+
+def main():
+    id_ranges = get_id_ranges(INPUT_FILE_LOCATION)
+    invalid_ids = []
+    sum_invalid_ids = 0
+    for id_range in id_ranges:
+        start_id = id_range[0]
+        end_id = id_range[1]
+        current_id = start_id
+        while current_id <= end_id:
+            if check_id_validity(current_id) == False:
+                invalid_ids.append(current_id)
+            current_id += 1
+    for id in invalid_ids:
+        sum_invalid_ids += id
+    print(sum_invalid_ids)
+
+main()
