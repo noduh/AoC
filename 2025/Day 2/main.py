a6afd9d6fb390349c4bf45b8c2db74101650cd0e
Noah Jorgensen <noduh.jorgensen@gmail.com>
2025-12-03 14:42:55 -0700

    added docstring and constant containing file location

--- 2025/Day 2/main.py
+++ 2025/Day 2/main.py
@@ -1,4 +1,14 @@
+INPUT_FILE_LOCATION = "2025/Day 2/input.txt"
+
 def get_ids(file_location: str) -> list[tuple[int, int]]:
+    """
+    Gets IDs from the given input file
+    
+    :param file_location: location of the file as a string
+    :type file_location: str
+    :return: list of ID ranges, where ranges are a tuple containing the IDs in the range
+    :rtype: list[tuple[int, int]]
+    """
     file_string = ""
     id_list = []
     with open(file_location, "r") as file:
