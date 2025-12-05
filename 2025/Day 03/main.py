33f8cfc8ac18d35c12d13843b605f9d98647e4c4
Noah Jorgensen <noduh.jorgensen@gmail.com>
2025-12-05 16:12:08 -0700

    find_largest_joltage now returns a string (for type safety) and uses recursion

--- 2025/Day 03/main.py
+++ 2025/Day 03/main.py
@@ -1,4 +1,5 @@
 INPUT_FILE_LOCATION = "2025/Day 03/input.txt"
+NUMBER_OF_BATTERIES_PER_BANK = 12
 
 
 def get_battery_banks(file_location: str) -> list[list[int]]:
@@ -26,35 +27,60 @@ def get_battery_banks(file_location: str) -> list[list[int]]:
     return banks
 
 
-def find_largest_joltage(bank: list[int]) -> int:
+def find_largest_joltage(
+    bank: list[int], max_size: int, starting_index: int = 0
+) -> str:
     """
     Finds the largest joltage in the given bank
 
     :param bank: the bank containing the batteries
     :type bank: list[int]
+    :param max_size: the amount of batteries to be turned on in the bank
+    :type max_size: int
+    :param starting_index: index to start searching the bank from
+    :type starting_index: int
     :return: the largest possible output joltage of the bank
     :rtype: int
     """
-    first_battery_joltage = 0
-    second_battery_joltage = 0
+    if max_size == 0:  # end of the recursion
+        return ""
+
     total_joltage = 0
-    second_battery_start_index = 0
-    for i in range(len(bank) - 1):  # only go to the second-to-last index
-        if bank[i] > first_battery_joltage:
-            first_battery_joltage = bank[i]
-            second_battery_start_index = i + 1
-    for i in range(second_battery_start_index, len(bank)):
-        if bank[i] > second_battery_joltage:
-            second_battery_joltage = bank[i]
-    total_joltage = int(str(first_battery_joltage) + str(second_battery_joltage))
-    return total_joltage
+    max_battery_joltage = 0
+    new_starting_index = 0
+    for i in range(starting_index, len(bank) - (max_size - 1)):
+        if bank[i] > max_battery_joltage:
+            max_battery_joltage = bank[i]
+            new_starting_index = i + 1
+    total_joltage = int(
+        str(total_joltage)
+        + str(max_battery_joltage)
+        + str(find_largest_joltage(bank, max_size - 1, new_starting_index))
+    )
+    return str(total_joltage)
+
+    # first_battery_joltage = 0
+    # second_battery_joltage = 0
+    # total_joltage = 0
+    # second_battery_start_index = 0
+    # for i in range(len(bank) - 1):  # only go to the second-to-last index
+    #     if bank[i] > first_battery_joltage:
+    #         first_battery_joltage = bank[i]
+    #         second_battery_start_index = i + 1
+    # for i in range(second_battery_start_index, len(bank)):
+    #     if bank[i] > second_battery_joltage:
+    #         second_battery_joltage = bank[i]
+    # total_joltage = int(str(first_battery_joltage) + str(second_battery_joltage))
+    # return total_joltage
 
 
 def main():
     battery_banks = get_battery_banks(INPUT_FILE_LOCATION)
     total_output_joltage = 0
     for bank in battery_banks:
-        total_output_joltage += find_largest_joltage(bank)
+        total_output_joltage = total_output_joltage + int(
+            find_largest_joltage(bank, NUMBER_OF_BATTERIES_PER_BANK)
+        )
     print(total_output_joltage)
 
 
