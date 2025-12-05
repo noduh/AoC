b0f73ff5d8c7ebf64384c9de01584dbabdc13a54
Noah Jorgensen <noduh.jorgensen@gmail.com>
2025-12-05 10:55:32 -0700

    created function for turning battery banks into a list of lists

---
+++ 2025/Day 03/main.py
@@ -0,0 +1,18 @@
+INPUT_FILE_LOCATION = "2025/Day 03/input.txt"
+
+
+def get_battery_banks(file_location: str) -> list[list[int]]:
+    banks_string = []
+    banks = []
+    with open(file_location, "r") as file:
+        for line in file:
+            banks_string.append(line)
+    for bank_string in banks_string:
+        bank_length = len(bank_string)
+        bank_num = int(bank_string)
+        bank = []
+        for i in range(2, bank_length + 1):
+            bank.append(bank_num // (10 ** (bank_length - i)))
+            bank_num %= 10 ** (bank_length - i)
+        banks.append(bank)
+    return banks
